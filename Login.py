044452237d9670e1afc8f9a1dbdc5b6a2e731c51
Prateek Ranjan <prateekranjan14059827@gmail.com>
2020-02-12 13:57:59 +0530

    improvised

--- Login.py
+++ Login.py
@@ -1,19 +1,18 @@
 email=input("Enter Your Email")
-if '@' in email:
-#paste
+if '@' in email:                                                                         #paste
                 
 password=input("Enter your password")
 
 if email=="hit@gmail.com" and password=="1234":
                 print("Welcome")
 elif email=="hit@gmail.com" and password!="1234":
-                print("Incorrect password") #if password is incorrect
-                password=input("Enter your password again") #re enter password
+                print("Incorrect password")                                     #if password is incorrect
+                password=input("Enter your password again")        #re enter password
                 if password=="1234":
                                 print("Welcome")
                 else:
-                                print("bhai tumse na ho payega")
+                                print("Incorrect credentials")
 else:
-                print("incorrect credentials")
+                print("Incorrect credentials")
 else:
-                print("Gaawar")
+                print("Maximum Attempts exceeded")
