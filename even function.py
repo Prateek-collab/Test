18f8cd6d83192e46963eb5c0d4365f1563403eb3
Prateek Ranjan <prateekranjan14059827@gmail.com>
2020-02-13 12:37:37 +0530

    Even function added

---
+++ even function.py
@@ -0,0 +1,11 @@
+def is_even(number):
+                """
+                this function tells if a given number is odd or even
+                created by someone
+                last edited 22/01/2020
+                output odd/even
+                """
+if number%2 == 0:
+                return "even"
+else:
+                return "odd"
